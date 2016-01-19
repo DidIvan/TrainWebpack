fa7153b6036415ad9ef5f66ce5b6b7d754e22209
Ivan <Holy.vinil@gmail.com>
2016-01-19 16:47:30 +0200

    s

---
+++ home.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Horbynko on 19.01.2016.
+ */
+'use strict';
+
+let welcome = require('./welcome');
+welcome('home');
---
+++ welcome.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Horbynko on 19.01.2016.
+ */
+'use strict';
+
+module.exports = function(message){
+    alert('welcome${message}');
+};
